9122189afa26718201592ce4a8d13304d030f988
beanb1123 <103089678+beanb1123@users.noreply.github.com>
2024-01-23 19:36:02 +0100

    Create registry.js

---
+++ registry.js
@@ -0,0 +1,13 @@
+let reg = [{ base: "start", quote: [ { key: "start", value: 0000 } ] }]
+
+let b = "start2";
+let q = { key: "start2", value: 1111 };
+
+for(let i = 1; i < 10; i++) {
+    reg[i].base = b;
+    for(let x = 1; x < 5; x++) {
+        reg[i].quote[x] = q;
+    }
+}
+
+console.log(JSON.stringify(reg));
